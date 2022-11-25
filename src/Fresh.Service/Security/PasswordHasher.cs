e84e98f202a2a795cdce518f9d4fa11e4baf3cea
Samandar <samandar.lcl@gmail.com>
2022-11-26 03:22:30 +0500

    Security tools added

---
+++ src/Fresh.Service/Security/PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using F.Service.Interfaces.Security;
+using Fresh.Service.Interfaces.Security;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresh.Service.Security
+{
+    internal class PasswordHasher : IPasswordHasher
+    {
+        public (string passwordHash, string salt) Hash(string password)
+        {
+            string salt = GenerateSalt();
+            string strongpassword =salt + password;
+            string hash = BCrypt.Net.BCrypt.HashPassword(strongpassword);
+            return (passwordHash: hash,salt: salt);
+        }
+
+        public bool Verify(string password, string salt, string hash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
