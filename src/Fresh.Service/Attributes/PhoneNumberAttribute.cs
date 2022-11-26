95fbda7e5f2894659e2da93590d8bb7e79b9b4ef
Samandar <samandar.lcl@gmail.com>
2022-11-26 05:27:09 +0500

    PhoneNumber checker updated

--- src/Fresh.Service/Attributes/PhoneNumberAttribute.cs
+++ src/Fresh.Service/Attributes/PhoneNumberAttribute.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Fresh.Service.Attributes
 {
-    internal class PhoneNumberAttribute
+    public static class PhoneNumberAttribute
     {
+        public static bool IsValid(string phoneNumber)
+        {
+            return new Regex(@"^[+998]{4}[0-9]{9}$").IsMatch(phoneNumber);
+        }
     }
 }
