9156296d14e152d4e8a85f45e6cf5eccb447ea09
Samandar <samandar.lcl@gmail.com>
2022-11-26 05:13:17 +0500

    added password checker

--- src/Fresh.Service/Attributes/PasswordAttribute.cs
+++ src/Fresh.Service/Attributes/PasswordAttribute.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Fresh.Service.Attributes
 {
-    internal class MediumPasswordAttribute
+    public static class PasswordAttribute
     {
+        public static string ValidationScore(this string password)
+        {
+            if (password.Length < 8 || password.Length > 18)
+                return "Password must be at least 8 characters and maximum 16 characters";
+            else if (new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$").IsMatch(password))
+                return "Medium";
+            else if (new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,18}$").IsMatch(password))
+                return "Strong";
+            else
+                return "Password must contain at least one uppercase letter,one number and one special character($!%*?&)";
+        }
     }
 }
