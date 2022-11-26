3cd98f1d0dc11938cfc0845f81d464a8c402e99a
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2022-11-26 14:18:58 +0500

    Added Repositories and constants , user entity, base entity,IGenericInterface

---
+++ src/Fresh.Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Fresh.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresh.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public bool IsAdmin { get; set; } 
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Salt { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string PassportSeria { get; set; } = string.Empty;
+    }
+}
