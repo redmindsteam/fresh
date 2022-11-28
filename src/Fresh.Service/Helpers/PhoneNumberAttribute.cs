75d842046f668020bab69afd24c802e6b17d6f27
Samandar <samandar.lcl@gmail.com>
2022-11-29 03:50:59 +0500

    Some files pages are changed and SingeltonClass User added

--- src/Fresh.Service/Helpers/PhoneNumberAttribute.cs
+++ src/Fresh.Service/Helpers/PhoneNumberAttribute.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Fresh.Service.Attributes
+namespace Fresh.Service.Helpers
 {
     public static class PhoneNumberAttribute
     {
