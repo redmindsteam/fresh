153b9afe51b962bfd7db7786ef03537262e6055f
Samandar <samandar.lcl@gmail.com>
2022-11-29 21:37:04 +0500

    Added failed update

--- src/Fresh.Service/Attributes/CurrentUserSingelton.cs
+++ src/Fresh.Service/Attributes/CurrentUserSingelton.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Fresh.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fresh.Service.Attributes
 {
-    internal class CurrentUserSingelton
+    public sealed class CurrentUserSingelton
     {
+        private CurrentUserSingelton() { }
+        private static User _currentUser = null;
+
+        public static User Instance
+        {
+            get { return _currentUser; }
+            set
+            {
+                if (_currentUser == null)
+                    _currentUser = value;
+            }
+        }
     }
 }
