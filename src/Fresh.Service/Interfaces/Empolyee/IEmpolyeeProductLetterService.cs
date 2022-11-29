5bda73d2479b6ff496fd5a6946567c1af67848cc
A S U S <mebeluz555520@gmail.com>
2022-11-29 15:11:34 +0500

    Created Service

---
+++ src/Fresh.Service/Interfaces/Empolyee/IEmpolyeeProductLetterService.cs
@@ -0,0 +1,13 @@
+﻿using Fresh.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresh.Service.Interfaces.Empolyee
+{
+    public interface IEmpolyeeProductLetterService : IGenericService<ProductLetter>
+    {
+    }
+}
