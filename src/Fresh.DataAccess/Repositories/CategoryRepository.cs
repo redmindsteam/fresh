3b4e6d335014857566d1fa09539164606e77b53d
Abdulaziz <muhammadaliyev01>
2022-11-29 14:31:06 +0500

    Added All Repositories

--- src/Fresh.DataAccess/Repositories/CategoryRepository.cs
+++ src/Fresh.DataAccess/Repositories/CategoryRepository.cs
@@ -3,6 +3,7 @@ using Fresh.Domain.Constants;
 using Fresh.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -13,29 +14,155 @@ namespace Fresh.DataAccess.Repositories
     public class CategoryRepository : ICategoryRepository
     {
         private readonly SQLiteConnection _con = new SQLiteConnection(DbConstants.CONNECTION_STRING);
-        public Task<bool> CreateAsync(Category item)
+        public async Task<bool> CreateAsync(Category item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = @"INSERT INTO categories(Name,Description) " +
+                     "VALUES($Name,$Description);";
+                var command = new SQLiteCommand(query, _con)
+                {
+                    Parameters =
+                    {
+                        new SQLiteParameter("Id",item.Id),
+                        new SQLiteParameter("Name", item.Name),
+                        new SQLiteParameter("Description", item.Description)
+                    }
+                };
+
+                var result = await command.ExecuteNonQueryAsync();
+                return result > 0;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<bool> DeleteAsync(int id)
+        public async Task<bool> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"DELETE FROM categories where id = {id}";
+                var command = new SQLiteCommand(query, _con);
+                var result = await command.ExecuteNonQueryAsync();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+
+            }
+            catch
+            {
+
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<IList<Category>> GetAllAsync(int skip, int take)
+        public async Task<IList<Category>> GetAllAsync(int skip, int take)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                var categories = new List<Category>();
+                await _con.OpenAsync();
+                string query = $"SELECT * FROM categories  lIMIT {take} OFFSET {skip};";
+                var command = new SQLiteCommand(query, _con);
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    var category = new Category()
+                    {
+                        Id = reader.GetInt32("id"),
+                        Name = reader.GetString("Name"),
+                        Description = reader.GetString("Description")
+
+                    };
+                    categories.Add(category);
+                }
+
+                return categories;
+            }
+            catch
+            {
+
+                return new List<Category>();
+            }
+            finally { _con.Close(); }
         }
 
-        public Task<Category> GetByIdAsync(int id)
+        public async Task<Category> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"select * from categories where Id = {id}";
+                SQLiteCommand command = new SQLiteCommand(query, _con);
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
+                {
+                    return new Category()
+                    {
+                        Id = reader.GetInt32("Id"),
+                        Name = reader.GetString("Name"),
+                        Description = reader.GetString("Description")
+                    };
+                }
+                else return null!;
+            }
+            catch
+            {
+
+                return null!;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<bool> UpdateAsync(int id, Category entity)
+        public async Task<bool> UpdateAsync(int id, Category entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"update categories set " +
+                    "Name = $Name, Description = $Description " +
+                    $"Where Id = {id}";
+                SQLiteCommand command = new SQLiteCommand(query, _con)
+                {
+                    Parameters =
+                    {
+
+                        new SQLiteParameter("Name", entity.Name),
+                        new SQLiteParameter("Description",  entity.Description)
+                    }
+                };
+                int result = await command.ExecuteNonQueryAsync();
+                if (result == 0)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
     }
 }
--- src/Fresh.DataAccess/Repositories/CheckRepository.cs
+++ src/Fresh.DataAccess/Repositories/CheckRepository.cs
@@ -3,6 +3,7 @@ using Fresh.Domain.Constants;
 using Fresh.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -13,29 +14,164 @@ namespace Fresh.DataAccess.Repositories
     public class CheckRepository : ICheckRepository
     {
         private readonly SQLiteConnection _con = new SQLiteConnection(DbConstants.CONNECTION_STRING);
-        public Task<bool> CreateAsync(Check item)
+        public async Task<bool> CreateAsync(Check item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = @"INSERT INTO Checks(CheckDescription,TotalSum,UserId,Date)" +
+                     "VALUES($CheckDescription,$TotalSum,$UserId,$Date);";
+                var command = new SQLiteCommand(query, _con)
+                {
+                    Parameters =
+                    {
+                        new SQLiteParameter("CheckDescription", item.CheckDescription),
+                        new SQLiteParameter("TotalSum", item.TotalSum),
+                        new SQLiteParameter("UserId", item.UserId),
+                        new SQLiteParameter("Date", item.Date)
+                    }
+                };
+
+                var result = await command.ExecuteNonQueryAsync();
+                return result > 0;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<bool> DeleteAsync(int id)
+        public async Task<bool> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"DELETE FROM Checks where id = {id}";
+                var command = new SQLiteCommand(query, _con);
+                var result = await command.ExecuteNonQueryAsync();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+
+            }
+            catch
+            {
+
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<IList<Check>> GetAllAsync(int skip, int take)
+        public async Task<IList<Check>> GetAllAsync(int skip, int take)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                var checks = new List<Check>();
+                await _con.OpenAsync();
+                string query = $"SELECT * FROM Checks  lIMIT {take} OFFSET {skip};";
+                var command = new SQLiteCommand(query, _con);
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    var check = new Check()
+                    {
+                        Id = reader.GetInt32("Id"),
+                        CheckDescription = reader.GetString("CheckDescription"),
+                        TotalSum = reader.GetFloat("TotalSum"),
+                        UserId = reader.GetInt32("UserId"),
+                        Date = reader.GetDataTypeName("Date")
+
+                    };
+                    checks.Add(check);
+                }
+
+                return checks;
+            }
+            catch
+            {
+
+                return new List<Check>();
+            }
+            finally { _con.Close(); }
         }
 
-        public Task<Check> GetByIdAsync(int id)
+        public async Task<Check> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"select * from Checks where Id = {id}";
+                SQLiteCommand command = new SQLiteCommand(query, _con);
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
+                {
+                    return new Check()
+                    {
+                        Id = reader.GetInt32("Id"),
+                        CheckDescription = reader.GetString("CheckDescription"),
+                        TotalSum = reader.GetFloat("TotalSum"),
+                        UserId = reader.GetInt32("UserId"),
+                        Date = reader.GetDataTypeName("Date")
+                    };
+                }
+                else return null!;
+            }
+            catch
+            {
+
+                return null!;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
 
-        public Task<bool> UpdateAsync(int id, Check entity)
+        public async Task<bool> UpdateAsync(int id, Check entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _con.OpenAsync();
+                string query = $"update Users set " +
+                    "CheckDescription = $CheckDescription, TotalSum = $TotalSum," +
+                    "UserId = $UserId," +
+                    " Date = $Date" +
+                    $"Where Id = {id}";
+                SQLiteCommand command = new SQLiteCommand(query, _con)
+                {
+                    Parameters =
+                    {
+
+                        new SQLiteParameter("CheckDescription", entity.CheckDescription),
+                        new SQLiteParameter("TotalSum", entity.TotalSum),
+                        new SQLiteParameter("UserId", entity.UserId),
+                        new SQLiteParameter("Date", entity.Date)
+                    }
+                };
+                int result = await command.ExecuteNonQueryAsync();
+                if (result == 0)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+            finally
+            {
+                _con.Close();
+            }
         }
     }
 }
