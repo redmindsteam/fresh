22bc0c50f18f627766d55fcfe0cbde6a295600cf
A S U S <mebeluz555520@gmail.com>
2022-11-29 19:40:35 +0500

    Cretae EmpolyeeRegisterService

---
+++ src/Fresh.Service/Director/DirectorFinanceService.cs
@@ -0,0 +1,14 @@
+﻿using Fresh.Service.Interfaces.DirectorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresh.Service.Director
+{
+    public class DirectorFinanceService : IDirectorFinanceService
+    {
+
+    }
+}
