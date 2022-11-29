ce76dc25a83269a37084da0468b4d669b812c18c
Samandar <samandar.lcl@gmail.com>
2022-11-29 21:06:18 +0500

    Login page beckend updated and UserValidation method added

--- src/Fresh.Service/Director/DirectorRegisterService.cs
+++ src/Fresh.Service/Director/DirectorRegisterService.cs
@@ -1,4 +1,6 @@
-﻿using Fresh.Domain.Entities;
+﻿using Fresh.DataAccess.Repositories;
+using Fresh.Domain.Entities;
+using Fresh.Service.Attributes;
 using Fresh.Service.Interfaces.DirectorService;
 using System;
 using System.Collections.Generic;
@@ -29,10 +31,35 @@ namespace Fresh.Service.Director
         {
             throw new NotImplementedException();
         }
-
         public Task<bool> UpdateAsync(int id, User entity)
         {
             throw new NotImplementedException();
+        }
+        public async Task<(string error,bool result)> UserValidation(string identifier, string password)
+        {
+            try
+            {
+                UserRepository userRepository = new UserRepository();
+                Security.PasswordHasher hasher = new Security.PasswordHasher();
+                User user = await userRepository.GetByEmail(identifier);
+               /// if (user == null) userRepository.GetByPhoneNumber(identifier);
+                if (user != null)
+                {
+                    if (hasher.Verify(password, user.Salt, user.PasswordHash))
+                    {
+                        CurrentRegistrar.Registrar = user;
+                        return (string.Empty, true);
+                    }
+                    else
+                        return ("Incorrect password", false);
+                }
+                else
+                    return ("User not found", false);
+            }
+            catch
+            {
+                return ("Something went error",false);
+            }
         }
     }
 }
