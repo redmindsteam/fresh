5d3f9da2c02d03e2fa325437e47ec9f272a18256
A S U S <mebeluz555520@gmail.com>
2022-11-29 15:39:05 +0500

    Created Service

---
+++ src/Fresh.Service/Interfaces/DirectorService/IDirectorProductLetterService.cs
@@ -0,0 +1,13 @@
+﻿using Fresh.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fresh.Service.Interfaces.DirectorService
+{
+    public interface IDirectorProductLetterService : IGenericService<ProductLetter>
+    {
+    }
+}
