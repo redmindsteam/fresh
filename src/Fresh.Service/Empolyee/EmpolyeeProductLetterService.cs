5726b5f7f9c691914dff3c071bed9fa9505aec68
A S U S <mebeluz555520@gmail.com>
2022-11-30 10:00:03 +0500

    Create Method EmpolyeeRegisterService

--- src/Fresh.Service/Empolyee/EmpolyeeProductLetterService.cs
+++ src/Fresh.Service/Empolyee/EmpolyeeProductLetterService.cs
@@ -1,10 +1,6 @@
 ﻿using Fresh.Domain.Entities;
 using Fresh.Service.Interfaces.EmpolyeeService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Fresh.Service.Empolyee
 {
