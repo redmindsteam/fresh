c2421e45c095de95ae959eb8baeb49803fd0a41a
Sanjar Mavlonov <sanjarmavlonov9@gmail.com>
2022-12-02 00:24:23 +0500

    Added Check

---
+++ src/Fresh.Desktop/Pages/Check.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fresh.Desktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for Check.xaml
+    /// </summary>
+    public partial class Check : Page
+    {
+        public Check()
+        {
+            InitializeComponent();
+        }
+    }
+}
