3e3a6e04e3d79ff67aa1ec07c87a8db87450336c
A S U S <mebeluz555520@gmail.com>
2022-12-02 21:08:28 +0500

    Create WindowCassa

--- src/Fresh.Desktop/Windows/Cassa.xaml.cs
+++ src/Fresh.Desktop/Windows/Cassa.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Fresh.Desktop.Windows
 {
@@ -10,6 +11,48 @@ namespace Fresh.Desktop.Windows
         public Cassa()
         {
             InitializeComponent();
+            ObservableCollection<CassaData> datas = new ObservableCollection<CassaData>();
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+            datas.Add(new CassaData { Name = "Sariyog", KgL = " Kg", Price = "20 000", Thunumber = "2", Summa = "40 000" });
+
+            cassaDataGrid.ItemsSource = datas;
+        }
+
+        
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public class CassaData
+        { 
+            public string Name { get; set; }
+            public string KgL { get; set; }
+            public string Price { get; set; }
+            public string Thunumber { get; set; }
+            public string Summa { get; set; }
+        }
+
+        private void RadioButton_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
         }
     }
 }
