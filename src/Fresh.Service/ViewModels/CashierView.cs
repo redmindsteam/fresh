70f96a79f38111abea91389e8468e431267af1d0
Abdulaziz <muhammadaliyev01>
2022-12-05 19:50:52 +0500

    Add cashier method created

--- src/Fresh.Service/ViewModels/CashierView.cs
+++ src/Fresh.Service/ViewModels/CashierView.cs
@@ -13,6 +13,8 @@ namespace Fresh.Service.ViewModels
         public string FullName { get; set; }=string.Empty;
 
         public string Email { get; set; }= string.Empty;
+        
+        public string Password { get; set; }= string.Empty;
 
         public string PhoneNumber { get; set; } = string.Empty;
 
