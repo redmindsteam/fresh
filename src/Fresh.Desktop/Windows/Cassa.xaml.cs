e7856852279321ec49ae3e418a694eb05ddf5ab1
A S U S <mebeluz555520@gmail.com>
2022-12-07 20:33:36 +0500

    Update

--- src/Fresh.Desktop/Windows/Cassa.xaml.cs
+++ src/Fresh.Desktop/Windows/Cassa.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using static Fresh.Desktop.Windows.Cassa;
 
 namespace Fresh.Desktop.Windows
 {
@@ -10,13 +11,19 @@ namespace Fresh.Desktop.Windows
     /// </summary>
     public partial class Cassa : Window
     {
+        public static ObservableCollection<CassaData> cassaDatas = new ObservableCollection<CassaData>();
+
+        public double price { get; private set; } = 0;
         public Cassa()
         {
             InitializeComponent();
+            refreshDataGridsss();
+        }
 
-            ObservableCollection<CassaData> cassaDatas = new ObservableCollection<CassaData>();
+        public void refreshDataGridsss()
+        {
+           
             cassaDatas.Add(new CassaData { Name = "Kolbasa", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
-        
             cassaDatas.Add(new CassaData { Name = "Kolbasa", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
             cassaDatas.Add(new CassaData { Name = "Asal", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
             cassaDatas.Add(new CassaData { Name = "Kolbasa", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
@@ -45,9 +52,13 @@ namespace Fresh.Desktop.Windows
             cassaDatas.Add(new CassaData { Name = "Kolbasa", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
             cassaDatas.Add(new CassaData { Name = "Kolbasa", KgL = "Kg", Price = "20.000", Thenumber = "2", Money = "40 000" });
 
-            cassaDataGrid.ItemsSource = cassaDatas;
+            
         }
 
+        public async void DataGridRefresh()
+        {
+            cassaDataGrid.ItemsSource = cassaDatas;
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -64,17 +75,14 @@ namespace Fresh.Desktop.Windows
             public string Money { get; set; }
         }
 
-        private void cassaDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
 
-        }
 
-        private void btn_close(object sender, RoutedEventArgs e)
+        private async void btn_close(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
         {
             if (cassaDataGrid.Items != null)
             {
@@ -82,27 +90,34 @@ namespace Fresh.Desktop.Windows
             }
         }
 
-        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        private async void btnBuy_Click(object sender, RoutedEventArgs e)
         {
-            double price = 0;
+            
            
             for (int i = 0; i < cassaDataGrid.Items.Count; i++)
-            { 
-                var res = (CassaData)cassaDataGrid.Items[i];
-                MessageBox.Show($"{res.Name}");
+            {
+                price += 0;
             }
             
             MessageBox.Show($"{price}");
         }
 
-        private void btnDelet_Click(object sender, RoutedEventArgs e)
-        {
 
+        private async void DataGrid_Load(object sender, RoutedEventArgs e)
+        {
+            DataGridRefresh();
         }
 
-        private void Delete_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private async void btnDeletd_Click(object sender, RoutedEventArgs e)
         {
-           
+            var item = (CassaData)cassaDataGrid.SelectedItem;
+            cassaDatas.Remove(item);
+            DataGridRefresh();
+        }
+
+        private async void WrapPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
         }
     }
 }
