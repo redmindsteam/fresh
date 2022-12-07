aa667446b89c5330dd89337e647dc2c751d2fb5b
Samandar <samandar.lcl@gmail.com>
2022-12-08 00:43:32 +0500

    Mini bugs sloved

--- src/Fresh.Desktop/Windows/Main.xaml.cs
+++ src/Fresh.Desktop/Windows/Main.xaml.cs
@@ -34,9 +34,7 @@ namespace Fresh.Desktop.Windows
         }
         private void rdConsignmentLetters_Click(object sender, RoutedEventArgs e)
         {
-            Cassa cassa = new Cassa();
-            cassa.Show();
-            //PagesNavigation.Navigate(new System.Uri("Pages/ConsignmentLettersPage.xaml", UriKind.RelativeOrAbsolute));
+            PagesNavigation.Navigate(new System.Uri("Pages/ConsignmentLettersPage.xaml", UriKind.RelativeOrAbsolute));
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
@@ -61,6 +59,11 @@ namespace Fresh.Desktop.Windows
         {
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
+        }
+
+        private void rdConsignmentLetters_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
