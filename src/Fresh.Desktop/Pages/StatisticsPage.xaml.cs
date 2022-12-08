36c4c03e6c31670d2570781c8ba50f2a4bbb0ee8
Zulaykho Saparbaeva <saparbaevazulaykho18@gmail.com>
2022-12-08 16:33:37 +0500

    Data grids changed

--- src/Fresh.Desktop/Pages/StatisticsPage.xaml.cs
+++ src/Fresh.Desktop/Pages/StatisticsPage.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static Fresh.Desktop.Windows.Cassa;
+using static Fresh.Desktop.Pages.StatisticsPage;
 
 namespace Fresh.Desktop.Pages
 {
